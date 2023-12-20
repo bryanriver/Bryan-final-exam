f2cd3282185ebf020249c5de652818347deb6b2b
bryanriver <122397008+bryanriver@users.noreply.github.com>
2023-12-20 12:00:29 -0800

    it wont let me play the scene

--- Player.gd
+++ Player.gd
@@ -4,12 +4,14 @@ extends Area2D
 
 
 func _process(delta):
-	var velocity = Vector2.ZERO
-	
-	if Input.is.action_pressed("move_right"):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	if Input.is.action_pressed("move_left"):
-		velocity.x += 1
-	if Input.is.action_pressed("move_up"):
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
 		
 	
